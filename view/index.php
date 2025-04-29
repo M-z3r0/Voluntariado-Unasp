fab31c45153c707268497a340c2a536c94bc8638
Murilo Cavalcanti <munery0205@gmail.com>
2025-04-28 22:19:03 -0300

    commit 1 - criação dos arquivos das views

--- view/index.php
+++ view/index.php
@@ -1,1 +1,27 @@
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial - Voluntariados</title>
+</head>
+<body>
+    <header>
+        
+    </header><!--Header-->
+    <main>
+        <section>
+
+        </section><!--Container - Contéudo 1-->
+        <section>
+
+        </section><!--Container - Doação-->
+        <section>
+
+        </section><!--Container - Sobre nós-->
+    </main><!--Content containers-->
+    <footer>
+
+    </footer><!--Footer-->
+</body>
+</html>
