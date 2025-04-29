e5a6363463586c4c891067b21cff6681a3769ff7
Murilo Cavalcanti <munery0205@gmail.com>
2025-04-29 01:00:46 -0300

    commit 2 - Design incial do login & conexao com bd

---
+++ public/scripts/login.js
@@ -0,0 +1,19 @@
+/*
+    Relacionado ao design
+*/
+
+const containter = document.getElementById('container')
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click',()=>{
+    containter.classList.add("active");
+});
+
+loginBtn.addEventListener('click',()=>{
+    containter.classList.remove("active");
+});
+
+/*
+    Lógica de validação
+*/
--- view/loginViews/login.php
+++ view/loginViews/login.php
@@ -3,9 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../../public/styles/login.css">
+    <title>Página de Login</title>
 </head>
 <body>
-    
+    <section class="container" id="container">
+        <div class="form-container sign-up">
+            <form action="">
+                <h1>Criação de conta</h1>
+                <input type="text" name="user_reg_name" id="user_reg_name" placeholder="Nome...">
+                <input type="text" name="user_reg_nickname" id="user_reg_nickname" placeholder="Nome de usuário...">
+                <input type="email" name="user_reg_email" id="user_reg_email" placeholder="Email...">
+                <h4 class="emailFeedBack"></h4>
+                <input type="password" name="user_reg_password" id="user_reg_password" placeholder="Senha...">
+                <ul>
+                    <li>A senha deve conter no mínimo 8 caracteres</li>
+                    <li>A senha deve conter letras e números [a1b2]</li>
+                    <li>A senha deve conter letras maiúsculas e minusculas [aAbB]</li>
+                    <li>A senha deve conter caracteres especiais [@&%$]</li>
+                </ul>
+                <input type="password" name="user_reg_confirmedpassword" id="user_reg_confirmedpassword" placeholder="Confirmação de senha...">
+                <h4 class="confirmedpasswordFeedBack"></h4>
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="">
+                <h1>Entrar na sua conta</h1>
+                <input type="email" name="user_reg_email" id="user_email" placeholder="Email...">
+                <input type="password" name="user_reg_password" id="user_password" placeholder="Senha...">
+                <a href="#">Esqueci minha senha</a>
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Bem vindo de volta!</h1>
+                    <p>Entre usando seu email e senha de cadastro</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Seja bem vindo!</h1>
+                    <p>Preencha os campos para finalizar seu registero</p>
+                    <button class="hidden" id="register">Sign up</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="../../public/scripts/login.js"></script>
 </body>
 </html>
